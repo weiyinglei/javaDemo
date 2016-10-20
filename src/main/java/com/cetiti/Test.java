a32944c9aff9157b87d28ad854a5678891f903f5
weiyinglei <weiyinglei@live.com>
2016-10-20 20:37:06 +0800

    log4j

---
+++ src/main/java/com/cetiti/Test.java
@@ -0,0 +1,14 @@
+/**
+ * Created by weiyinglei on 2016-10-19.
+ */
+package com.cetiti;
+
+import org.apache.log4j.Logger;
+
+public class Test {
+    private static Logger LOGGER = Logger.getLogger(Test.class);
+
+    public static void main(String[] args) {
+        LOGGER.error("Hello World!");
+    }
+}
