fd5d2a86215539b291d128c79faa029e881b14dd
weiyinglei <weiyinglei@live.com>
2016-10-21 17:16:04 +0800

    stringbuffer

--- src/main/java/com/cetiti/stringbuffer/StringBufferDemo.java
+++ src/main/java/com/cetiti/stringbuffer/StringBufferDemo.java
@@ -1,11 +1,12 @@
 package com.cetiti.stringbuffer;
 
+import java.util.Locale;
+
 /**
  * Created by weiyinglei on 2016-10-20.
  */
 public class StringBufferDemo {
     public static void main(String[] args) {
         StringBuffer buffer = new StringBuffer();
-        new String();
     }
 }
